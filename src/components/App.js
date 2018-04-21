0a34637ed324f276d4f5f2e7ab086286f376e288
Pablo <freakaziod210@gmail.com>
2018-04-21 06:13:21 -0600

    Moved App component to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,52 @@
-import React, { Component } from "react";
+import React from "react";
 import Card from "material-ui/Card";
 import Button from "material-ui/Button";
 import AddIcon from "@material-ui/icons/Add";
 import BudgetModal from "./BudgetModal";
 
-class App extends Component {
-  handleOpen = () => {
-    this.props.onModalOpen();
+const App = ({ onModalOpen, onModalClose, isModalOpen }) => {
+  const handleOpen = () => {
+    onModalOpen();
   };
 
-  handleClose = () => {
-    this.props.onModalClose();
+  const handleClose = () => {
+    onModalClose();
   };
 
-  render() {
-    const { isModalOpen } = this.props;
-    return (
-      <div>
-        <Card
-          raised
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: 100,
-            width: 200,
-            borderRadius: 8
-          }}
-        >
-          <div className="App">This app is working</div>
-        </Card>
-        <Button
-          variant="fab"
-          color="primary"
-          onClick={this.handleOpen}
-          style={{ position: "fixed", right: 30, bottom: 30 }}
-        >
-          <AddIcon />
-        </Button>
-        <BudgetModal
-          isOpen={isModalOpen}
-          onClose={this.handleClose}
-          values={{
-            name: " Budget Name",
-            description: "Budget Description",
-            amount: "123456789"
-          }}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Card
+        raised
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: 100,
+          width: 200,
+          borderRadius: 8
+        }}
+      >
+        <div className="App">This app is working</div>
+      </Card>
+      <Button
+        variant="fab"
+        color="primary"
+        onClick={handleOpen}
+        style={{ position: "fixed", right: 30, bottom: 30 }}
+      >
+        <AddIcon />
+      </Button>
+      <BudgetModal
+        isOpen={isModalOpen}
+        onClose={handleClose}
+        values={{
+          name: " Budget Name",
+          description: "Budget Description",
+          amount: "123456789"
+        }}
+      />
+    </div>
+  );
+};
 
 export default App;
