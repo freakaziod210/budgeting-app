18d1b8370c06a171229d555690e1f8ea28dc986d
Pablo <freakaziod210@gmail.com>
2018-05-02 22:36:02 -0600

    Fix bug with deleting values

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -4,7 +4,12 @@ import App from "./App";
 
 const mapStateToProps = state => ({
   isModalOpen: state.app.isModalOpen,
-  budgetValues: state.form.currentBudget && state.form.currentBudget.values
+  budgetValues: (state.form.currentBudget &&
+    state.form.currentBudget.values) || {
+    budgetName: "",
+    budgetDescription: "",
+    budgetAmount: ""
+  }
 });
 
 const mapDispatchToProps = dispatch => ({
