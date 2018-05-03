fd1f4c8a5acc620545d24e06b29a2efd8ee4ff7e
Pablo <freakaziod210@gmail.com>
2018-05-02 22:32:05 -0600

    Refactored app.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,45 +4,35 @@ import Button from "material-ui/Button";
 import AddIcon from "@material-ui/icons/Add";
 import BudgetModal from "./BudgetModal";
 
-const App = ({ onModalOpen, onModalClose, isModalOpen, budgetValues }) => {
-  const handleOpen = () => {
-    onModalOpen();
-  };
-
-  const handleClose = () => {
-    onModalClose();
-  };
-
-  return (
-    <div>
-      <Card
-        raised
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: 100,
-          width: 200,
-          borderRadius: 8
-        }}
-      >
-        <div className="App">This app is working</div>
-      </Card>
-      <Button
-        variant="fab"
-        color="primary"
-        onClick={handleOpen}
-        style={{ position: "fixed", right: 30, bottom: 30 }}
-      >
-        <AddIcon />
-      </Button>
-      <BudgetModal
-        budgetValues={budgetValues}
-        isOpen={isModalOpen}
-        onClose={handleClose}
-      />
-    </div>
-  );
-};
+const App = ({ onModalOpen, onModalClose, isModalOpen, budgetValues }) => (
+  <div>
+    <Card
+      raised
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 100,
+        width: 200,
+        borderRadius: 8
+      }}
+    >
+      <div className="App">This app is working</div>
+    </Card>
+    <Button
+      variant="fab"
+      color="primary"
+      onClick={() => onModalOpen()}
+      style={{ position: "fixed", right: 30, bottom: 30 }}
+    >
+      <AddIcon />
+    </Button>
+    <BudgetModal
+      budgetValues={budgetValues}
+      isOpen={isModalOpen}
+      onClose={() => onModalClose()}
+    />
+  </div>
+);
 
 export default App;
