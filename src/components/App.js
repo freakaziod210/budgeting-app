ad90433c1e53b4b9a1b16549dd4ac4d48902f396
Pablo <freakaziod210@gmail.com>
2018-05-11 00:48:27 -0600

    Center using a hack

--- src/components/App.js
+++ src/components/App.js
@@ -1,69 +1,69 @@
 import React from "react";
 import Card from "material-ui/Card";
+import Grid from 'material-ui/Grid';
 import Button from "material-ui/Button";
 import AddIcon from "@material-ui/icons/Add";
 import BudgetModal from "./BudgetModal";
 
 const App = ({
-  onModalOpen,
-  onModalClose,
-  isModalOpen,
-  budgetFormValues,
-  onBudgetSelect,
-  onBudgetClear,
-  onAddBudget,
-  budget: { budgets, currentBudget }
+    onModalOpen,
+    onModalClose,
+    isModalOpen,
+    budgetFormValues,
+    onBudgetSelect,
+    onBudgetClear,
+    onAddBudget,
+    budget: { budgets, currentBudget }
 }) => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center"
-    }}
-  >
-    <div
-      style={{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexWrap: "wrap"
-      }}
-    >
-      {budgets.map((b, i) => (
-        <Card
-          key={`${b.name}-${i}`}
-          raised
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            height: 150,
-            width: 300,
-            borderRadius: 4,
-            margin: 10
-          }}
+        <div
+            style={{
+                display: "flex",
+            }}
         >
-          <div>{`${b.name}-${i}`}</div>
-          <div>{b.description}</div>
-          <div>{b.amount}</div>
-        </Card>
-      ))}
-    </div>
-    <Button
-      variant="fab"
-      color="primary"
-      onClick={() => onModalOpen()}
-      style={{ position: "fixed", right: 30, bottom: 30 }}
-    >
-      <AddIcon />
-    </Button>
-    <BudgetModal
-      onSubmit={onAddBudget}
-      budgetFormValues={budgetFormValues}
-      isOpen={isModalOpen}
-      onClose={() => onModalClose()}
-    />
-  </div>
-);
+            <div style={{ flex: 1 }} />
+            <div
+                style={{ flex: 8, display: 'flex', flexWrap: 'wrap' }}
+            >
+                {budgets.map((b, i) => (
+                    <div
+                        key={`${b.name}-${i}`}
+                    >
+                        <Card
+                            raised
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                height: 150,
+                                width: 300,
+                                borderRadius: 4,
+                                margin: 15
+                            }}
+                        >
+                            <div>{`${b.name}-${i}`}</div>
+                            <div>{b.description}</div>
+                            <div>{b.amount}</div>
+                        </Card>
+                    </div>
+                ))}
+            </div>
+            <div style={{ flex: 1 }} />
+            <Button
+                variant="fab"
+                color="primary"
+                onClick={() => onModalOpen()}
+                style={{ position: "fixed", right: 30, bottom: 30 }}
+            >
+                <AddIcon />
+            </Button>
+            <BudgetModal
+                onSubmit={onAddBudget}
+                budgetFormValues={budgetFormValues}
+                isOpen={isModalOpen}
+                onClose={() => onModalClose()}
+            />
+        </div>
+    );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -10,38 +10,175 @@ import budgetReducer from "./reducers/budgetReducer";
 import { reducer as formReducer } from "redux-form";
 
 const rootReducer = combineReducers({
-  app: appReducer,
-  form: formReducer,
-  budget: budgetReducer
+    app: appReducer,
+    form: formReducer,
+    budget: budgetReducer
 });
 
 const initialState = {
-  app: { isModalOpen: false },
-  form: {
-    budgetForm: {
-      values: {
-        name: "",
-        description: "",
-        amount: ""
-      }
+    app: { isModalOpen: false },
+    form: {
+        budgetForm: {
+            values: {
+                name: "",
+                description: "",
+                amount: ""
+            }
+        }
+    },
+    budget: {
+        currentBudget: {},
+        budgets: [
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+
+            {
+                amount: "12345",
+                description: "New Description",
+                name: "New Name"
+            },
+        ]
     }
-  },
-  budget: {
-    currentBudget: {},
-    budgets: []
-  }
 };
 
 const store = createStore(
-  rootReducer,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    rootReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const App = () => (
-  <Provider store={store}>
-    <AppContainer />
-  </Provider>
+    <Provider store={store}>
+        <AppContainer />
+    </Provider>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
